6d4a1cc09cab7f9936faea6abd3f4bb67c0ff439
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-02-06 04:21:15 +0000

    debugged subtraction

--- app.rb
+++ app.rb
@@ -25,6 +25,7 @@ get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
+  @result = @second_num - @first_num
   erb(:sub_result)
 end
 
